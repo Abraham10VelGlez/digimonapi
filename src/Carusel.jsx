5b769f88f9625fc19455eed0b0b55fa2706bdacd
Abraham10VelGlez <usbrifa@gmail.com>
2024-07-12 16:32:01 -0600

    updatecarusel

--- src/Carusel.jsx
+++ src/Carusel.jsx
@@ -10,7 +10,7 @@ import Hook_list from './Hook_list';
 
 
 export default function Carusel() {
-    const { digimons } = Hook_list()
+    const { digimons } = Hook_list();
     const [monster, setmonster] = useState([]);
     const responsiveOptions = [
         {
@@ -38,30 +38,26 @@ export default function Carusel() {
     useEffect(() => {
         setmonster(digimons);
         //console.log(digimons);
-
-
     }, [digimons]);
 
     const productTemplate = (monster) => {
         return (
-            <div className="border-round m-2 text-center py-2 px-1">
+            <div className="custom-card border-round m-2 text-center py-2 px-1">
                 <div className="mb-3">
-                    <img src={monster.image} alt={monster.name} className="w-6 shadow-2" />
+                    <img src={monster.image} alt={monster.name} className="custom-image w-6 shadow-2" />
                 </div>
                 <div>
-                    <h4 className="mb-1">{monster.name}</h4>
-                    <h6 className="mt-0 mb-3">${monster.name}</h6>
-
+                    <h4 className="mb-1 custom-name">{monster.name}</h4>
+                    <h6 className="mt-0 mb-3 custom-price">${monster.name}</h6>
                 </div>
             </div>
         );
     };
 
-
     return (
         <div className="card">
-            <Carousel value={monster} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={productTemplate} />
+            <Carousel    value={monster} numVisible={3} numScroll={3} responsiveOptions={responsiveOptions} itemTemplate={productTemplate} />
         </div>
-    )
+    );
 }
 
